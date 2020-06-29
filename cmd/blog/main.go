d9bc48da47820ca1f03279e9f15ed9a9f91850d2
paulurbina <paulurbina94@gmail.com>
2020-06-29 15:28:35 -0500

    created repository

---
+++ cmd/blog/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/paulurbina/small_blog-go/internal/data"
+	"github.com/paulurbina/small_blog-go/internal/server"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	serv, err := server.New(port)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// connection to the database.
+	d := data.New()
+	if err := d.DB.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	// start the server.
+	go serv.Start()
+
+	// Wait for an in interrupt.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	// Attempt a graceful shutdown.
+	serv.Close()
+	data.Close()
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/paulurbina/small_blog-go
+
+go 1.14
+
+require (
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.7.0
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+)
